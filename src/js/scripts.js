c3cd3d75a4c1cc2774b7b1ee46dd7168f07ca2c1
César Mejía <cesar.mejia@bancoazteca.com>
2021-07-12 21:01:34 -0500

    Add alert when close button is clicked

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -286,7 +286,7 @@ $digits.eq(0).focus();
 // Initialize close buttons.
 $('.screen header .close-btn').on('click', function () {
     console.log('scripts.js: Closing WebView');
-    $('.detail-screen main').prepend('typeof closeMoviePayment' + typeof closeMoviePayment);
+    $('.detail-screen main').prepend('typeof closeMoviePayment: ' + typeof closeMoviePayment);
     if (typeof closeMoviePayment === 'function' || typeof closeMoviePayment === 'object') {
         closeMoviePayment();
     }
