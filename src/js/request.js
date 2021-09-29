89051eae25ff0c98c871152122248b97834f22cf
César Mejía <cesar.mejia@bancoazteca.com>
2021-09-29 12:29:01 -0500

    Se cambiaron endpoints a producción

--- src/js/request.js
+++ src/js/request.js
@@ -9,7 +9,8 @@ const request = {
      * @param {function} error 
      */
     buyMovie: function (idMovie, body, headers, success, error) {
-        const url = 'https://apigateway.superappbaz.com/integracion/superapp/diversion/gestion-peliculas/v1/peliculas/' + idMovie + '/compras';
+        // const url = 'https://apigateway.superappbaz.com/integracion/superapp/diversion/gestion-peliculas/v1/peliculas/' + idMovie + '/compras';
+        const url = 'https://api.baz.app/superapp/diversion/gestion-peliculas/v1/peliculas/' + idMovie + '/compras';
         request.send(url, 'POST', body, headers, success)
     },
 
@@ -18,7 +19,8 @@ const request = {
      * @param {string} idOperation 
      */
     movieById: function(idOperation, success, error) {
-        const url = 'https://apigateway.superappbaz.com/integracion/superapp/canal/clientes/gestion-sesiones/v1/portal/operaciones/' + idOperation;
+        // const url = 'https://apigateway.superappbaz.com/integracion/superapp/canal/clientes/gestion-sesiones/v1/portal/operaciones/' + idOperation;
+        const url = 'https://api.baz.app/superapp/canal/clientes/gestion-sesiones/v1/portal/operaciones/' + idOperation;
         request.send(url, 'GET', undefined, {
             'x-sicu': '3bad1290ac4600a569162efaa09117ea',
             'x-id-interaccion': '123e4567-e89b-12d3-a456-426655440000',
@@ -34,7 +36,8 @@ const request = {
      * @param {function} error
      */
     paymentButton: function (body, headers, success, error) {
-        const url = 'https://apigateway.superappbaz.com/integracion/superapp/pagos/captacion/traspasos/v1/boton-pago';
+        // const url = 'https://apigateway.superappbaz.com/integracion/superapp/pagos/captacion/traspasos/v1/boton-pago';
+        const url = 'https://api.baz.app/integracion/superapp/pagos/captacion/traspasos/v1/boton-pago';
         request.send(url, 'POST', body, headers, success, error);
     },
 
