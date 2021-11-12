c645d9b66729dac0169b6b7cdb50cc3152c78abb
César Mejía <cesar.mejia@bancoazteca.com>
2021-11-11 18:01:50 -0600

    Finish reorganizing code

--- src/js/movies.js
+++ src/js/movies.js
@@ -7,11 +7,63 @@ let globals = {};
  * Show resume screen.
  */
 const showResumeScreen = () => {
-  const { headers } = globals;
-  const body = { transaccion: { ...globals.datosFlujo.botonPago } }; // Aquí me quedé
+  const { headers, datosFlujo } = globals;
+  const { idPelicula, botonPago, tokenOperacion, numeroCuentaClienteCadenaBaz, nombrePelicula } = datosFlujo;
+  const body = { transaccion: { ...botonPago, tokenOperacion } };
 
   showLoader(true);
-  request.paymentButton(body, headers, () => {}, showErrorScreen);
+  request.paymentButton(body, headers, (data) => {
+    const { resultado: { fechaOperacion, horaOperacion, numeroMovimiento } } = data;
+    const body = { transaccion: { fechaOperacion, numeroMovimiento } };
+    const buyMovieHeaders = { ...headers };
+
+    delete buyMovieHeaders["x-id-operacion-conciliacion"];
+
+    request.buyMovie(idPelicula, body, buyMovieHeaders, (data) => {
+      const { folio } = data;
+
+      bind($(".resume-screen"), {
+        fecha: `${fechaOperacion}, ${horaOperacion}`,
+        precio: botonPago.detallePago.montoEnvio,
+        tarjeta: numeroCuentaClienteCadenaBaz,
+        nombre: nombrePelicula,
+        folio: folio 
+      });
+
+      // Attach click handler to change the view.
+      $(".resume-screen .movie-btn").on("click", showSuccessScreen);
+
+      changeScreen($(".resume-screen"));
+      showLoader(false);
+    }, showErrorScreen);
+  }, showErrorScreen);
+}
+
+/**
+ * Show success screen.
+ */
+const showSuccessScreen = () => {
+  console.log(`movies.js: ${getMobileOperatingSystem()} detected.`);
+
+  const { idPelicula, nombrePelicula, imagenPelicula } = globals.datosFlujo;
+  const seeMovieHref = getMobileOperatingSystem() === "iOS"
+    ? `gssapp://sapp?flowName=GSIFMod&data=${btoa(idPelicula)}`
+    : `intent://?verPelicula?data=${btoa(idPelicula)}#Intent;scheme=sappdl;action=android.intent.action.VIEW;S.browser_fallback_url=https%3A%2F%2Fplay.google.com/store/apps/details?id=mx.app.baz.superapp;end`;
+
+  bind($(".success-screen"), { nombre: nombrePelicula });
+  $(".success-screen").find(".see-movie-href").attr("href", seeMovieHref);
+
+  // Wait for image is already loaded.
+  if (imagenPelicula) {
+    $(`<img src="${imagenPelicula}">`).on("load", function () {
+      $(".success-screen")
+        .find('[data-id="imagen"]')
+        .attr("style", `background-image: url(${imagenPelicula})`)
+        .addClass("displayed");
+    });
+  }
+
+  changeScreen($(".success-screen"));
 }
 
 export const initMovies = (data) => {
